8d7d7476f6bfae99d8bc3de66b2916aacf075a71
lostmeta <l0stm3ta@gmail.com>
2026-06-22 22:08:16 +0200

    lmpsgen v1.1

--- core/generate.py
+++ core/generate.py
@@ -2,7 +2,12 @@ import itertools
 def generate_wordlist(base_words,combo_limit,leet_mode,spec_char):
     wordlist = set()
     leet = {"a": "4","A": "4","i": "1","I": "1","e": "3","E": "3","o": "0","O": "0"}
-    special_char = ["!","@","#","$","?"]
+    if spec_char == 'default':
+        special_char = ["!","@","#","$","?"]
+    elif spec_char:
+        special_char = list(spec_char)
+    else:
+        special_char = []
     table = str.maketrans(leet)
     for r in range(1,combo_limit + 1):
         for combo in itertools.permutations(base_words,r):
@@ -10,23 +15,23 @@ def generate_wordlist(base_words,combo_limit,leet_mode,spec_char):
             variants = {word,word.lower(),word.capitalize(),word.title()}
             for variant in variants:
                 wordlist.add(variant)
-                if leet_mode in ["yes","y"]:
+                if leet_mode:
                     wordlist.add(variant.translate(table))
                 for char in special_char:
-                    if spec_char in ["yes","y"]:
+                    if spec_char:
                         wordlist.add(f"{variant}{char}")
-                        if leet_mode in ["yes","y"]:
+                        if leet_mode:
                             wordlist.add(f"{variant.translate(table)}{char}")
     for num in range(1,10000):
         for word2 in base_words:
             variants = {word2,word2.lower(),word2.capitalize(),word2.title()}
             for variant in variants:
                 wordlist.add(f"{variant}{num}")
-                if leet_mode in ["yes","y"]:
+                if leet_mode:
                     wordlist.add(f"{variant.translate(table)}{num}")
                 for char in special_char:
-                    if spec_char in ["yes","y"]:
+                    if spec_char:
                         wordlist.add(f"{variant}{num}{char}")
-                        if leet_mode in ["yes","y"]:
+                        if leet_mode:
                             wordlist.add(f"{variant.translate(table)}{num}{char}")
     return wordlist
--- lmpsgen.py
+++ lmpsgen.py
@@ -5,30 +5,20 @@ def person_main(args):
     verb = 0
     if args.verbose:
         verb = 1
-    target_first_name = input("Target First name: ").strip()
-    target_last_name = input("Target Last name: ").strip()
-    target_birthday = input("Target birthday (DD-MM-YYYY): ").strip()
-    partner_name = input("Target Partner name: ").strip()
-    pet_name = input("Pet Name: ").strip()
-    additional_words = input("additional words: ").strip()
-    spec_char = input("Do you want to add special chars at the end of words?: ").strip()
-    leet_mode = input("Leet mode?: ").strip()
-    
-    day,month,year = target_birthday.split("-")
-    base_words = [day,month,year]
-    base_words += parse_input(target_first_name)
-    base_words += parse_input(target_last_name)
-    base_words += parse_input(partner_name)
-    base_words += parse_input(pet_name)
-    base_words += parse_input(additional_words)
-
+    base_words = []
+    base_words += parse_input(args.year)
+    base_words += parse_input(args.name)
+    base_words += parse_input(args.lastname)
+    base_words += parse_input(args.pet)
+    base_words += parse_input(args.additional_words)
+    wordlist_name = args.output
     str_wordl = 0
-    wordlist = generate_wordlist(base_words,args.combo,leet_mode,spec_char)
-    with open("wordlist.txt","w") as f:
+    wordlist = generate_wordlist(base_words,args.combo,args.leatspeak,args.spec_char)
+    with open(wordlist_name,"w") as f:
         f.write("")
     for wordl in wordlist:
         str_wordl += 1
-        with open("wordlist.txt", "a") as f:
+        with open(wordlist_name, "a") as f:
             f.write(f"{wordl}\n")
             if verb == 1:
                 if args.verbose:
@@ -40,7 +30,17 @@ def person_main(args):
     print(f"wordlist saved in wordlist.txt")
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Generating wordlists")
+    parser.add_argument('-n','--name',dest='name',help="Target's First name")
+    parser.add_argument('-l','--lastname',dest='lastname',help="Target's Last name")
+    parser.add_argument('-y','--year',dest='year',help="Target's birth year or important dates")
+    parser.add_argument('-o','--output',dest='output',help="Path to the output file (default: wordlist.txt")
+    parser.add_argument('-p','--pet',dest='pet',help="Target's pet name")
+    parser.add_argument('-a','--additional',dest='additional_words',help="Additional custom keywords, separated by comma")
     parser.add_argument('-v','--verbose',dest='verbose',action='store_true',help="Enable verbose output (detailed logging)")
     parser.add_argument('-c',dest='combo', default=2,type=int,help="Combination depth (e.g., -c 2 chains two words together)")
+    parser.add_argument('-ls','--leatspeak',dest='leatspeak',action='store_true',help='Enable Leetspeak mutations (e.g., a->4, e->3)')
+    parser.add_argument('-sc', '--specialchar', dest='spec_char', nargs='?', const='default',
+                    help="Add special characters. Use without values for default (!@#$?) or specify custom (e.g., -sc '!_')")
+
     args = parser.parse_args()
     person_main(args)
